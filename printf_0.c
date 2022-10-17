59c0d4e3ba8dec4d1691697a2b9e48a835904018
CeeBuda <sinnocent506@gmail.com>
2022-10-17 15:37:25 +0000

    Adding files

---
+++ printf_0.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * _printf - produces formated output and prints to stdout
+ * @format: specifies format for arguments
+ *
+ * Return: returns the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+
+}
